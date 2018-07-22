d98d9e7d093bf0a4e321c4744320557f3a68c80f
Atsushi Eno <atsushieno@gmail.com>
2018-07-22 11:46:53 +0900

    Add netcoreapp2.1 library for .NET Core (experimental).

---
+++ Commons.Music.Midi.DesktopShared/MidiAccessManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Commons.Music.Midi
+{
+	public partial class MidiAccessManager
+	{
+		partial void InitializeDefault ()
+		{
+			// What we can support in this implementation assembly: WinMM and ALSA. For Mac, you need Commons.Music.Midi.XamMacFull.csproj.
+			Default = Environment.OSVersion.Platform != PlatformID.Unix ? (IMidiAccess) new WinMM.WinMMMidiAccess () : new Alsa.AlsaMidiAccess ();
+		}
+	}
+}
