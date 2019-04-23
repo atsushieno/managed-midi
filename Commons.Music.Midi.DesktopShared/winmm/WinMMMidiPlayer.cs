2825b15bcfe691acd37692f743bb4e0c9ee4f469
Atsushi Eno <atsushieno@gmail.com>
2019-04-24 00:11:18 +0900

    Obsolete WinMMMidiPlayer.

--- Commons.Music.Midi.DesktopShared/winmm/WinMMMidiPlayer.cs
+++ Commons.Music.Midi.DesktopShared/winmm/WinMMMidiPlayer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 namespace Commons.Music.Midi.WinMM
 {
+	[Obsolete ("This class does not do anything special. Just use MidiPlayer.")]
 	public class WinMMMidiPlayer : MidiPlayer
 	{
 		public WinMMMidiPlayer (MidiMusic music)
