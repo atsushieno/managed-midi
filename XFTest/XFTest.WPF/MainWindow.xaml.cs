27effe9c07ecf755c42e0b98f18f7c9ef74fcc02
jaladaGmbH <andreas.krebs@jalada.de>
2020-09-20 21:24:29 +0200

    Add Wpf test project

---
+++ XFTest/XFTest.WPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Commons.Music.Midi.WinMM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace XFTest.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MidiSystem.Initialize();
+            
+            Forms.Init();
+            
+            LoadApplication(new XFTest.App());
+        }
+    }
+}
