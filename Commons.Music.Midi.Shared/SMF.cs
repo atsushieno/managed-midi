4c9d0406ab25348fd12f08061e8abecb672e2fad
Nytra <14206961+Nytra@users.noreply.github.com>
2025-09-28 10:09:44 +0100

    Match code style of surrounding code

--- Commons.Music.Midi.Shared/SMF.cs
+++ Commons.Music.Midi.Shared/SMF.cs
@@ -315,38 +315,32 @@ namespace Commons.Music.Midi
 		{
 			int i = index;
 			int end = index + size;
-			while (i < end) 
-			{
-				if (bytes[i] >= 128)
-				{
-					// New status byte
+			while (i < end) {
+				if (bytes[i] >= 128) {
 					runningStatus = bytes[i];
-					if (runningStatus == SysEx1)
-					{
-						yield return new MidiEvent(SysEx1, 0, 0, bytes, index, size);
+					if (bytes [i] == 0xF0) {
+						yield return new MidiEvent(0xF0, 0, 0, bytes, index, size);
 						i += size;
-						continue;
+					} else {
+						var z = MidiEvent.FixedDataSize (bytes [i]);
+						if (end < i + z)
+							throw new Exception (string.Format (
+								"Received data was incomplete to build MIDI status message for '{0:X}' status.",
+								bytes [i]));
+						yield return new MidiEvent (bytes [i],
+							(byte) (z > 0 ? bytes [i + 1] : 0),
+							(byte) (z > 1 ? bytes [i + 2] : 0),
+							null, 0, 0);
+						i += z + 1;
 					}
+				} else {
 					var z = FixedDataSize(runningStatus);
 					if (end < i + z)
-						throw new Exception (string.Format (
-							"Received data was incomplete to build MIDI status message for '{0:X}' status.",
-							runningStatus));
-					yield return new MidiEvent (runningStatus,
-						(byte) (z > 0 ? bytes [i + 1] : 0),
-						(byte) (z > 1 ? bytes [i + 2] : 0),
-						null, 0, 0);
-					i += z + 1;
-				}
-				else
-				{
-					// Is using running status
-					var z = FixedDataSize(runningStatus);
-					if (end < i + z)
-					{
 						throw new Exception($"Received data was incomplete to build MIDI running status message for '{runningStatus:X}' status.");
-					}
-					yield return new MidiEvent(runningStatus, bytes[i], (byte)((z > 1) ? bytes[i + 1] : 0), null, 0, 0);
+					yield return new MidiEvent(runningStatus,
+						bytes[i], 
+						(byte)((z > 1) ? bytes[i + 1] : 0), 
+						null, 0, 0);
 					i += z;
 				}
 			}
